746251262b228880b7b78ae0511c1fe8148b956e
LL <763184581@qq.com>
2017-08-08 21:09:58 +0800

    creatBaseVC

---
+++ LLViewController/LLViewController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LLViewController
+//
+//  Created by wanglulu on 2017/8/8.
+//  Copyright © 2017年 wanglulu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LLViewController/LLViewController/BaseViewController.h
@@ -0,0 +1,13 @@
+//
+//  BaseViewController.h
+//  LLViewController
+//
+//  Created by wanglulu on 2017/8/8.
+//  Copyright © 2017年 wanglulu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BaseViewController : UIViewController
+
+@end
